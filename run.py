86481ee8b99502168e8d0232a9eaafbb3b20471e
vincent <ododovincent54@gmail.com>
2019-06-14 15:58:20 +0300

    added a function to create user and to create new account in my run.py file

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 import string,random,time
 from locker import Credentials,UsersData
+def new_account(id,user_name,password):
+    '''
+    Function to creating new account
+    '''
+    new_user = Credentials(id,user_name,password)
+    return new_user
+def create_user(user):
+    '''
+    Function that saves the user's credentials
+    '''
+    user.create_account()
