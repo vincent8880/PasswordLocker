c182d1cf1631cb7170d7047e34af063401f4870b
vincent <ododovincent54@gmail.com>
2019-06-14 15:31:15 +0300

    created a new method

--- locker.py
+++ locker.py
@@ -4,4 +4,24 @@ class Credentials:
     class that creates new accounts and authenticates the user
     '''
     users list =[]
-    def _init_
+    def _init_(self, identify,user_name,password):
+        self.identify = identify
+        self.user_name = user_name
+        self.password = password
+    def create_account(self):
+        '''
+        function to create a new account
+        '''
+        Crendentials.users_list.append(self)
+    @class method
+    def authenticate_account(cls, name, key):
+        '''
+        method for authenticating created account
+        '''
+        for user in cls.users_list:
+             if user.user_name == name and user.password == key:
+                
+                return user
+            return 0
+
+    
